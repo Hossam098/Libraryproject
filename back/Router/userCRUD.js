4bc45fc447fb552e38ee1d48c430b2d5cf67f0bd
Nader Mamdouh <74846059+NaderMamdouh68@users.noreply.github.com>
2023-08-11 17:28:05 +0300

    nader

--- back/Router/userCRUD.js
+++ back/Router/userCRUD.js
@@ -50,6 +50,51 @@ user.get('/getuser',
     }
 );
 
+user.put('/updateuser',
+    checkUser,
+    upload.single('image'),
+    async (req, res) => {
+        let error = [];
+        try {
+            const sqlSelect = "SELECT * FROM users WHERE id = ?";
+            const result = await query(sqlSelect, [req.id]);
+            if (result.length > 0) {
+                let image = result[0].image;
+                if (req.file) {
+                    image = req.file.filename;
+                }
+                const userDate = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    image: image,
+                    phone: req.body.phone,
+                    national_id: req.body.national_id,
+                    uni: req.body.uni,
+                    faculty: req.body.faculty,
+                    department: req.body.department,
+                    nationalaty: req.body.nationalaty,
+                }
+                const sqlUpdate = "UPDATE users SET ? WHERE id = ?";
+                const resultUpdate = await query(sqlUpdate, [userDate, req.id]);
+                if (resultUpdate.affectedRows > 0) {
+                    return res.status(200).json({ message: "User updated successfully" });
+                } else {
+                    error.push("User not updated");
+                    return res.status(400).json({ message: error });
+                }
+            } else {
+                error.push("No user found");
+                return res.status(400).json({ message: error });
+            }
+        } catch (errors) {
+            error.push(errors);
+            return res.status(500).json({ message: error });
+        }
+    }
+);
+
+                    
+
 
 
 export default user;
