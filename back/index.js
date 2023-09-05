75fa00759d7c781946949e3896c95b94afc8e438
Nader Mamdouh <74846059+NaderMamdouh68@users.noreply.github.com>
2023-09-05 17:14:22 +0300

    nader

---
+++ back/index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import query from './Database/DBConnection.js';
+import user from './Router/userCRUD.js';
+import userAuth from './Authentication/userAuth.js';
+import serPayment from './Router/serPayment.js';
+import serviceStepTwo from './Router/serviceStepTwo.js';
+import managerAuth from './Authentication/managerAuth.js';
+import manager from './Router/manager.js';
+
+
+const app = express();
+app.use(express.json());
+
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+  optionsSuccessStatus: 200
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+    key: 'user',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: false,
+
+    }
+}));
+
+dotenv.config({ path: './.env' });
+
+app.use(bodyParser.json());
+app.use(express.static('public/imgs'));
+
+app.use('', serPayment);
+app.use('', serviceStepTwo);
+// app.use('', serviceStepTwoEdit);
+
+
+app.use('/auth', userAuth);
+app.use('/authmanager', managerAuth);
+app.use('/user', user);
+app.use('/manager', manager);
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 5000");
+});
