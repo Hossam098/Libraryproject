aa150198a7389e2aaa312fc85dea1ae01e8a261f
Nader Mamdouh <74846059+NaderMamdouh68@users.noreply.github.com>
2023-09-20 17:48:22 +0300

    nader

--- back/Router/Admin.js
+++ back/Router/Admin.js
@@ -27,6 +27,24 @@ Admin.get('/getallApplicants',
         }
     }
 )
+Admin.get('/getallApplicantsShow',
+    checkAdmin,
+    async (req, res) => {
+        let error = [];
+        try {
+            const sqlSelect = `SELECT * FROM submit `;
+            const result = await query(sqlSelect);
+            if (result.length > 0) {
+                return res.status(200).json(result);
+            } else {
+                return res.status(200).json({ message: "لا يوجد طلبات" });
+            }
+        } catch (errors) {
+            error.push(errors);
+            return res.status(500).json({ message: error });
+        }
+    }
+)
 
 
 
