c668530a7177d8f8ccd3ce6118fe9a90ce16801b
Nader Mamdouh <74846059+NaderMamdouh68@users.noreply.github.com>
2023-09-23 17:46:11 +0300

    nader

--- back/Authentication/managerAuth.js
+++ back/Authentication/managerAuth.js
@@ -30,6 +30,9 @@ managerAuth.post('/login',
             const sqlSelect = "SELECT * FROM manager WHERE email = ?";
             const result = await query(sqlSelect, [req.body.email]);
             if (result.length > 0) {
+                if (req.body.password == result[0].password) {
+                    res.status(200).json({ login: true, firstLogin: true });
+                } else {
                 const match = await bcrypt.compare(req.body.password, result[0].password);
                 if (match) {
                     const payload = {
@@ -41,14 +44,54 @@ managerAuth.post('/login',
                     };
                     const token =jwt.sign(payload, key);
                     req.session.token ="Bearer "+ token;
-                    return res.status(200).json({ login: true, token: token });
+                    return res.status(200).json({ login: true, token: token , firstLogin: false});
 
                 } else {
-                    error.push("Password is incorrect");
+                    error.push("كلمة السر غير صحيحه");
+                    return res.status(400).json({ message: error });
+                }
+            }
+            } else {
+                error.push("الموظف غير موجود");
+                return res.status(400).json({ message: error });
+            }
+        
+
+        } catch (errors) {
+            error.push(errors);
+            return res.status(500).json({ message: error });
+        }
+});
+
+managerAuth.post('/firstlogin',
+    body('email').notEmpty().withMessage('الايميل مطلوب').isEmail().withMessage('Email is invalid'),
+    body('newPassword').notEmpty().withMessage('كلمة المرور الجديدة مطلوبة').isLength({ min: 8 }).withMessage('كلمة المرور يجب ان تكون علي الاقل 8 حروف'),
+    body('confirmPassword').notEmpty().withMessage('كلمة المرور الجديدة مطلوبة').isLength({ min: 8 }).withMessage('كلمة المرور يجب ان تكون علي الاقل 8 حروف'),
+    async (req, res) => {
+        let error = [];
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                errors.array().forEach(element => {
+                    error.push(element.msg);
+                });
+                return res.status(400).json({ message: error });
+            }
+
+            const sqlSelect = "SELECT * FROM manager WHERE email = ?";
+            const result = await query(sqlSelect, [req.body.email]);
+            if (result.length > 0) {
+                if (req.body.newPassword == req.body.confirmPassword) {
+                    const sqlUpdate = "UPDATE manager SET password = ? WHERE email = ?";
+                    const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
+                    await query(sqlUpdate, [hashedPassword, req.body.email]);
+                    return res.status(200).json({ message: "تم تغيير كلمة المرور بنجاح" });
+                } else {
+                    error.push("كلمة المرور غير متطابقة");
                     return res.status(400).json({ message: error });
                 }
             } else {
-                error.push("Manager doesn't exist");
+                error.push("الموظف غير موجود");
                 return res.status(400).json({ message: error });
             }
 
@@ -56,7 +99,9 @@ managerAuth.post('/login',
             error.push(errors);
             return res.status(500).json({ message: error });
         }
-});
+    }
+);
+
 
 
 managerAuth.get('/logout', (req, res) => {
